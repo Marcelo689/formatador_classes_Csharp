e019f746a27df5ae1f4288730958eaeef505bf4a
Aluno <marcelosj7159@gmail.com>
2023-09-28 18:40:20 -0300

    atualizando projeto

---
+++ assets/template.cs
@@ -0,0 +1,13 @@
+namespace assets{
+    public class Template{
+        public int Id {get;set;}
+        public string Nome {get;set;}
+        public DateTime Data {get;set;}
+        public decimal Salario {get;set;}
+        public double Peso {get;set;}
+        public float Altura {get;set;}
+        public string ExemploPalavraComposta {get;set;}
+        public string PalavraComTextoDescartavelViewModel {get;set;}
+        public string PalavraComAcentoPao {get;set;}
+    }
+}
