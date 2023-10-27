737910af71176fda4e3d3b9ef1f3878704e39472
Marcelo689 <marcelosj7159@gmail.com>
2023-10-27 05:19:48 -0300

    a

--- arquivo.cs
+++ arquivo.cs
@@ -14,3 +14,32 @@ public class Pessoa{
     public bool Vivo {get;set;}
 }
 
+namespace Fabrica.Web.Areas.Operacoes.Models.CadastrarTemplate{
+    public class Template{
+
+      [Display(ResourceType = typeof(Fabrica.Web.App_GlobalResources.Operacoes.CadastrarTemplate),
+        Name = nameof(Web.App_GlobalResources.Operacoes.CadastrarTemplate.labelId))]
+        public int Id { get;set; }
+
+      [Display(ResourceType = typeof(Fabrica.Web.App_GlobalResources.Operacoes.CadastrarTemplate),
+        Name = nameof(Web.App_GlobalResources.Operacoes.CadastrarTemplate.labelNome))]
+        public string Nome { get;set; }
+
+      [Display(ResourceType = typeof(Fabrica.Web.App_GlobalResources.Operacoes.CadastrarTemplate),
+        Name = nameof(Web.App_GlobalResources.Operacoes.CadastrarTemplate.labelSenha))]
+        public string Senha { get;set; }
+
+      [Display(ResourceType = typeof(Fabrica.Web.App_GlobalResources.Operacoes.CadastrarTemplate),
+        Name = nameof(Web.App_GlobalResources.Operacoes.CadastrarTemplate.labelEmail))]
+        public string Email { get;set; }
+    }
+        public static explicit operator Template(TemplateTO to)
+        {
+            return new Template{
+                Id = to.Id,
+                Nome = to.Nome,
+                Senha = to.Senha,
+                Email = to.Email,
+            };
+        }
+}
